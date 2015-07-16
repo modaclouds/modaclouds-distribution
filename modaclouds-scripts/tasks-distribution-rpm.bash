1e8e2b75fe5a087c09074d937b0e014a958db20b
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-07-16 16:24:06 +0300

    Minor update

--- modaclouds-scripts/tasks-distribution-rpm.bash
+++ modaclouds-scripts/tasks-distribution-rpm.bash
@@ -22,38 +22,38 @@ modaclouds-distribution@node-rpm@${_task} : \
 		mosaic-node-wui-rpm@${_task} \
 
 modaclouds-distribution@components-rpm@${_task} : \
+		modaclouds-platform-components-rpm@${_task} \
 		modaclouds-components-fuseki-rpm@${_task} \
-		modaclouds-components-monitoring-manager-rpm@${_task} \
-		modaclouds-components-monitoring-dda-rpm@${_task} \
-		modaclouds-components-monitoring-sda-matlab-rpm@${_task} \
-		modaclouds-components-monitoring-sda-weka-rpm@${_task} \
 		modaclouds-components-load-balancer-controller-rpm@${_task} \
 		modaclouds-components-load-balancer-reasoner-rpm@${_task} \
 		modaclouds-components-metric-explorer-rpm@${_task} \
 		modaclouds-components-metric-importer-rpm@${_task} \
+		modaclouds-components-monitoring-sda-matlab-rpm@${_task} \
+		modaclouds-components-monitoring-sda-weka-rpm@${_task} \
 		mosaic-components-couchdb-rpm@${_task} \
+		mosaic-components-me2cp-rpm@${_task} \
+		mosaic-components-mysql-rpm@${_task} \
 		mosaic-components-rabbitmq-rpm@${_task} \
 		mosaic-components-riak-kv-rpm@${_task} \
-		mosaic-components-mysql-rpm@${_task} \
-		mosaic-components-me2cp-rpm@${_task} \
 		mosaic-object-store-rpm@${_task} \
 
 modaclouds-distribution@services-rpm@${_task} : \
+		modaclouds-platform-services-rpm@${_task} \
+		modaclouds-services-fg-analyzer-rpm@${_task} \
+		modaclouds-services-fg-local-db-rpm@${_task} \
 		modaclouds-services-fuseki-rpm@${_task} \
-		modaclouds-services-monitoring-manager-rpm@${_task} \
-		modaclouds-services-monitoring-dda-rpm@${_task} \
-		modaclouds-services-monitoring-sda-matlab-rpm@${_task} \
-		modaclouds-services-monitoring-sda-weka-rpm@${_task} \
 		modaclouds-services-load-balancer-controller-rpm@${_task} \
 		modaclouds-services-load-balancer-reasoner-rpm@${_task} \
 		modaclouds-services-metric-explorer-rpm@${_task} \
 		modaclouds-services-metric-importer-rpm@${_task} \
 		modaclouds-services-models-at-runtime-rpm@${_task} \
+		modaclouds-services-monitoring-sda-matlab-rpm@${_task} \
+		modaclouds-services-monitoring-sda-weka-rpm@${_task} \
 		modaclouds-services-sla-core-rpm@${_task} \
-		modaclouds-services-fg-analyzer-rpm@${_task} \
-		modaclouds-services-fg-local-db-rpm@${_task} \
+		mosaic-object-store-rpm@${_task} \
 
 modaclouds-distribution@tools-rpm@${_task} : \
+		modaclouds-platform-tools-rpm@${_task} \
 		modaclouds-tools-mdload-rpm@${_task} \
 
 modaclouds-rpm@${_task} : \
